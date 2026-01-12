1132ee127008495abfd00c344d859b55d7fb2d26
cima-tech <cima.its@gmail.com>
2026-01-12 01:13:33 -0400

    Delete modules/core-modules.js

--- modules/core-modules.js
+++
@@ -1,4 +0,0 @@
-/* modules/core-modules.js - Módulos centrales del sistema */
-export { default as StorageService } from './storage-service.js';
-export { PATIENT_FIELD_CONFIG } from './patient-config.js';
-export { Views } from './views.js';
